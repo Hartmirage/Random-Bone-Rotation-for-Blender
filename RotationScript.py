a091bd188a54f16152a6e6db6cd7d764e3152b72
Hartmirage <o.kroker@live.de>
2020-01-15 19:31:33 +0100

    implementing Randomness in Hand and clothing

--- RotationScript.py
+++ RotationScript.py
@@ -2,11 +2,9 @@ import bpy
 import math
 import mathutils
 import os
+from random import randrange
 from random import uniform
 
-DEBUG = True
-
-WantedHand = 2 #for Hand1 to Hand6
 NumberOfPoses = 1 #Number of Renders and LabelFiles that will be created
 StartAt = 0 #if script was run before and you just want a few extra
 #Directory = 'D:/blender_projects/handscript/test' #Filepath in witch the output will be saved(windows)
@@ -20,6 +18,76 @@ hasWatch = False
 hasRing = False
 hasSleeve = False
 
+ActiveCollection = bpy.data.collections["Hand 1"]
+
+HandList = []
+
+class Hand:
+  def __init__(self, collection, watch, ring, cloth1, cloth2, cloth3, rig):
+    self.collection = collection
+    self.watch = watch
+    self.ring = ring
+    self.cloth1 = cloth1
+    self.cloth2 = cloth2
+    self.cloth3 = cloth3
+    self.rig = rig
+
+def initHands():
+    Hand1 = Hand(bpy.data.collections["Hand 1"],
+                        bpy.data.collections["Watch Hand 1"],
+                        bpy.data.collections["Ring Hand 1"],
+                        bpy.data.collections["Cloth 1 Hand 1"],
+                        bpy.data.collections["Cloth 2 Hand 1"],
+                        bpy.data.collections["Cloth 3 Hand 1"],
+                        bpy.data.objects["Rig_Hand_1"])
+    HandList.append(Hand1)
+    
+    Hand2 = Hand(bpy.data.collections["Hand 2"],
+                        bpy.data.collections["Watch Hand 2"],
+                        bpy.data.collections["Ring Hand 2"],
+                        bpy.data.collections["Cloth 1 Hand 2"],
+                        bpy.data.collections["Cloth 2 Hand 2"],
+                        bpy.data.collections["Cloth 3 Hand 2"],
+                        bpy.data.objects["Rig_Hand_2"])
+    HandList.append(Hand2)
+    
+    Hand3 = Hand(bpy.data.collections["Hand 3"],
+                        bpy.data.collections["Watch Hand 3"],
+                        bpy.data.collections["Ring Hand 3"],
+                        bpy.data.collections["Cloth 1 Hand 3"],
+                        bpy.data.collections["Cloth 2 Hand 3"],
+                        bpy.data.collections["Cloth 3 Hand 3"],
+                        bpy.data.objects["Rig_Hand_3"])
+    HandList.append(Hand3)
+    
+    Hand4 = Hand(bpy.data.collections["Hand 4"],
+                        None,
+                        bpy.data.collections["Ring Hand 4"],
+                        None,
+                        None,
+                        None,
+                        bpy.data.objects["Rig_Hand_4"])
+    HandList.append(Hand4)
+    
+    Hand5 = Hand(bpy.data.collections["Hand 5"],
+                        bpy.data.collections["Watch Hand 5"],
+                        bpy.data.collections["Ring Hand 5"],
+                        bpy.data.collections["Cloth 1 Hand 5"],
+                        bpy.data.collections["Cloth 2 Hand 5"],
+                        bpy.data.collections["Cloth 3 Hand 5"],
+                        bpy.data.objects["Rig_Hand_5"])
+    HandList.append(Hand5
+    )
+    Hand6 = Hand(bpy.data.collections["Hand 6"],
+                        bpy.data.collections["Watch Hand 6"],
+                        bpy.data.collections["Ring Hand 6"],
+                        bpy.data.collections["Cloth 1 Hand 6"],
+                        bpy.data.collections["Cloth 2 Hand 6"],
+                        None,
+                        bpy.data.objects["Rig_Hand_6"])
+    HandList.append(Hand6)
+
+#returns the Hand Rig of the Hand Collection that is visible
 def getActiveRig():
     for x in bpy.context.view_layer.objects:
         if x == bpy.data.objects["Rig_Hand_1"]:
@@ -35,34 +103,27 @@ def getActiveRig():
         if x == bpy.data.objects["Rig_Hand_6"]:
             return x
 
+def setActiveCollection(Rig):
+    global ActiveCollection
+    if Rig == bpy.data.objects["Rig_Hand_1"]:
+        ActiveCollection = bpy.data.collections["Hand 1"]
+    if Rig == bpy.data.objects["Rig_Hand_2"]:
+        ActiveCollection = bpy.data.collections["Hand 2"]
+    if Rig == bpy.data.objects["Rig_Hand_3"]:
+        ActiveCollection = bpy.data.collections["Hand 3"]
+    if Rig == bpy.data.objects["Rig_Hand_4"]:
+        ActiveCollection = bpy.data.collections["Hand 4"]
+    if Rig == bpy.data.objects["Rig_Hand_5"]:
+        ActiveCollection = bpy.data.collections["Hand 5"]
+    if Rig == bpy.data.objects["Rig_Hand_6"]:
+        ActiveCollection = bpy.data.collections["Hand 6"]
 
-def activeRig(RigNumber):
-    switcher = {
-                1:Rig1,
-                2:Rig2,
-                3:Rig3,
-                4:Rig4,
-                5:Rig5,
-                6:Rig6
-             }
-    return switcher.get(RigNumber,"Invalid Handnumber")
-
-#another aproach to determin the active Rig ##############################TODO
-def setActiveRig():
-    for x in bpy.context.view_layer.objects:
-        helpSetActiveRig(x.name)
-        
-        
-def helpSetActiveRig(Hand_Rig):
-    switcher = {
-                'Rig_Hand_1':Rig1,
-                'Rig_Hand_2':Rig2,
-                'Rig_Hand_3':Rig3,
-                'Rig_Hand_4':Rig4,
-                'Rig_Hand_5':Rig5,
-                'Rig_Hand_6':Rig6
-                }
-    ARig = switcher.get(Hand_Rig,"Invalid Rig")
+def randomizeClothing(Collection):
+    sleeve = randrange(5) #gets random integer between 0 and 4
+    ring = randrange(2)
+    watch = randrange(2)
+    
+    
 
 #rotates a single bone within the limits of the constaint
 def rotateBone(self):
@@ -99,12 +160,8 @@ def resetBones(Rig):
 #rotates all bones, renders the image and resets the bones
 def rotateAndRender(counter, Rig):
     rotateBones(Rig)
-    print('____________________')
-
     renderImage(counter)
-    
-    exportLabels(counter)
-    
+    exportLabels(counter, Rig)
     resetBones(Rig)
 
 #renders image and saves it at the given path at the top of the script
@@ -119,12 +176,11 @@ def renderImage(counter):
 def formatFloat(float):
     return '{:.12f}'.format(float)
 
-
 #writes name of bone, HeadLocationVector and TailLocationVector in txt file for each bone
-def exportLabels(counter):
+def exportLabels(counter, Rig):
     path = LabelsFilePath + '/' + str(counter) + '.txt'
     f = open(str(path), 'w')
-    for x in (ARig.pose.bones):
+    for x in (Rig.pose.bones):
         try:
             headX = formatFloat(x.head[0])
             HeadY = formatFloat(x.head[1])
@@ -152,17 +208,17 @@ def createDirectory(path):
 
 def main():
     #select ActiveRig
-    ARig = getActiveRig()
+    ActiveRig = getActiveRig()
     
     #set ARig to active Object to make sure that enabling Pose Mode is possible
-    bpy.context.view_layer.objects.active = ARig
+    bpy.context.view_layer.objects.active = ActiveRig
     #make sure to be in POSE mode, so the rotation will show in the rendered Image(in Object mode you won't see the difference)
     bpy.ops.object.mode_set(mode='POSE')
     
     #setting the Path Variables
     #needs to be global to get the FilePath Varibale from the top. leaving out "global" would create a new local Variable
     global FilePath
-    FilePath = Directory + '/' + ARig.name
+    FilePath = Directory + '/' + ActiveRig.name
     global ImageFilePath
     ImageFilePath = FilePath + '/images' #directory where the images will be saved
     global LabelsFilePath
@@ -175,12 +231,41 @@ def main():
     
     #loop for the wanted number of images
     for i in range(StartAt, StartAt + NumberOfPoses):
-        rotateAndRender(i, ARig)
+        rotateAndRender(i, ActiveRig)
         print(i)
         
-main()
+def hideExcept(Hand):
+    for x in HandList:
+        x.collection.hide_viewport = True
+    try:       
+        Hand.collection.hide_viewport = False
+    except:
+        print('Index out of Bounds')
+        
+def testing():
+    initHands()
+    L = bpy.context.view_layer
+    hideExcept(HandList[1])
+    
+    
+#main()
+testing()
 
  
 #kleidungsstücke zufällig und im export file labeln
 #links rechts labeln
 #tiefenbild(opencv)
+
+#letztlicher ablauf in pseudo:
+#Pfade setzen und ordner generiern
+#Hände(class) erstellen und in Liste adden
+#zufällig alle ausser einer Hand ausblenden
+#zufällig Kleidung, Ring und Uhr ein/ausblenden
+#Bones zufällig rotieren
+#Position der Bones, sowie Kleidung(bool) Uhr(bool) und Ring(bool) auslesen und labeln
+#Bild rendern
+#Bones zurückrotieren
+#das ganze loopen
+
+def generateData():
+    
