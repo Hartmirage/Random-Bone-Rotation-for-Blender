813b898a90951c7d87b36b2854a6cc9838bdf7d5
Hartmirage <o.kroker@live.de>
2020-02-24 17:44:25 +0100

    some images still rendering slow

--- RotationScript.py
+++ RotationScript.py
@@ -7,7 +7,7 @@ from random import randint
 from random import uniform
 
 #define all needed variables that are set by the user here -----------------------------------------------------------------------------------
-NumberOfPoses = 1 #Number of wanted results 
+NumberOfPoses = 10 #Number of wanted results 
 StartAt = 0 #if script was run before and an enhancement of the dataset is wanted, set the index accordingly.
 #if the dataset features the files 0 to 100 set the StartAt variable to 101.
 Directory = 'D:/blender_projects/handscript/test' #Filepath in witch the output will be saved(windows)-------------------------WINDOWS FORMAT
@@ -18,13 +18,12 @@ Directory = 'D:/blender_projects/handscript/test' #Filepath in witch the output 
 hasWatch = False
 hasRing = False
 hasSleeve = False
-
-ActiveCollection = bpy.data.collections["CollectionHand1"] #set ActiveCollection to Hand 1 at start
 HandList = []
+ImageFilePath = Directory + '/images' #directory where the images will be saved
+LabelsFilePath = Directory + '/labels' #directory where the labelfiles will b saved
 
-#call this before the mainloop
-#---------------------create Directories and everything that is used by the function ---------------------------------------------------------
-
+#--------------------------------------- START OF [BEFORE THE LOOP] ----------------------------------------------------------
+#
 #creates the Directory with a given path --- helpmethod for createDirectories
 def createDirectory(path):
     try:
@@ -37,14 +36,12 @@ def createDirectory(path):
 #this makes sure the main Directory exists, as well as the Directories for the renders and the labels
 def createDirectories():
     #create additional directories inside the mainpath for pictures and labels
-    ImageFilePath = Directory + '/images' #directory where the images will be saved
-    LabelsFilePath = Directory + '/labels' #directory where the labelfiles will b saved
     #take the defined filepaths for export and make sure they exist(create them)
     createDirectory(Directory)
     createDirectory(LabelsFilePath)
     createDirectory(ImageFilePath)
     
-#object to Hold information of Hand
+#Object that stores the rig, clothes, watch, ring and the parent collection of a Hand
 class Hand:
   def __init__(self, collection, watches, rings, clothes, rig):
     self.collection = collection
@@ -53,56 +50,76 @@ class Hand:
     self.clothes = clothes
     self.rig = rig
 
+#returns Substring from startIndex to endIndex of a given string
 def getSubString(string, startIndex, endIndex):
     returnString = ''
     for i in range(startIndex, endIndex):
         returnString += string[i]
     return returnString
 
+#returns a collection with a given name ---- used to get the Watch, Ring and Clothes of the corresponding HandCollection
 def getSubCollection(string):
     List = []
     for col in bpy.data.collections:
         if string in col.name:
             List.append(col)
-    return List  
+    return List  #the Collections are returned as an Array so the Clothes can be switched via an index, or in the future Rings and Watches can be switched
 
+#returns a Rig with a given name
 def getRig(string):
     for rig in bpy.data.objects:
         if string in rig.name:
             return rig
 
+#creates the Objects of Class Hand and stores them in the HandList
+#-----looks for a collection that is named like CollectionHand1, Rig like Hand1Rig, Watch like Hand1Watch etc.
 def initHands():
     for x in bpy.data.collections:
         if "CollectionHand" in x.name:
-            #HandList.append(Hand())
-            Name = getSubString(x.name, len(x.name)-5, len(x.name))
-            Collection = x
-            Watch = getSubCollection(Name + "Watch")
+            Name = getSubString(x.name, len(x.name)-5, len(x.name)) #this gets a String like "Hand1" to find all collections and the rig starting with "Hand1"
             HandList.append(Hand(x,
                                     getSubCollection(Name + "Watch"),
                                     getSubCollection(Name + "Ring"),
                                     getSubCollection(Name + "Cloth"),
                                     getRig(Name + "Rig")))
 
+#before the mainloop hide all hand collections
 def hideAllHands():
-    #before the mainloop hide all hand collections
     for x in HandList:
-        x.collection.hide_viewport = True      
+        hideHand(x)
 
 #this method need to run before the main loop to prepare all the collections and directories
 def init():    
     createDirectories()
     initHands()
     hideAllHands()
+#--------------------------------------- END OF [BEFORE THE LOOP] ----------------------------------------------------------
 
+#--------------------------------------- START OF [MAIN LOOP] --------------------------------------------------------------
+#
 
-#call this inside the mainloop
+#Hides the Collection of the given Hand in the Viewport
 def unhideOne(Hand):
-    Hand.collection.hide_viewport = False  
-    
-def hideHand(Hand):
-    Hand.collection.hide_viewport = True
+    Hand.collection.hide_render = False  
+    for i in Hand.watches:
+        i.hide_render = False
+    for i in Hand.rings:
+        i.hide_render = False
+    for i in Hand.clothes:
+        i.hide_render = False
 
+#unhides the collection in the Viewport    
+def hideHand(Hand):
+    Hand.collection.hide_render = True
+    for i in Hand.watches:
+        i.hide_render = True
+    for i in Hand.rings:
+        i.hide_render = True
+    for i in Hand.clothes:
+        i.hide_render = True
+
+#hides/unhides clothes, ring and watch based on randomness, but makes sure only one of each is enabled
+#sets the booleans for labelexport
 def randomize(Hand):
     #hide all watches, rings and sleeves
     for x in Hand.watches:
@@ -112,85 +129,39 @@ def randomize(Hand):
     for x in Hand.clothes:
         x.hide_viewport = True
     
+    #determines if the watch is shown or not
     numOfWatches = len(Hand.watches)
     randWatch = 0    
     if numOfWatches != 0:
         randWatch = randint(0, numOfWatches) 
     if randWatch != 0 and numOfWatches != 0:
-        Hand.watches[randWatch - 1].hide_viewport = False
+        Hand.watches[randWatch - 1].hide_render = False
         hasWatch = True
     else:
         hasWatch = False
     
+    #determines if the ring is shown or not
     numOfRings = len(Hand.rings)
     randRing = 0
     if numOfRings != 0:
         randRing = randint(0, numOfRings)
     if randRing != 0 and numOfRings != 0:
-        Hand.rings[randRing - 1].hide_viewport = False
+        Hand.rings[randRing - 1].hide_render = False
         hasRing = True
     else:
         hasRing = False
-        
+    
+    #determines if a sleeve is shown or not and which one of them    
     numOfClothes = len(Hand.clothes)
     randCloth = 0
     if numOfClothes != 0:
         randCloth = randint(0, numOfClothes)
     if randCloth != 0 and numOfClothes != 0:
-        Hand.clothes[randCloth - 1].hide_viewport = False
+        Hand.clothes[randCloth - 1].hide_render = False
         hasSleeve = True
     else:
         hasSleeve = False
     
-
-
-
-def prepareHand(index):
-    #choose one hand, set as active, unhide the collection of active hand
-    #hide/unhide clothes, ring and watch (randomized)
-    #rotate bones randomized within the limits of the constraints
-    pass
-    
-#####insert all methods helping the prepare method underneath
-
-#def export():
-    #export labels: right/left, hasWatch(bool), hasRing(bool), hasSleeve(bool)
-    #export labels for each bone(position, rotation??)
-    #render image and save
-    #render tiefenbild?
-    
-#####insert all methods helping the export method underneath
-
-#def reset():
-    #reset rotation of bones
-    #hide the active hand collection
-
-def loop():
-    #choose one Hand random
-    temp = len(HandList)
-    HandIndex = randrange(temp)
-    Hand = HandList[HandIndex]
-    #unhide it
-    unhideOne(Hand)
-    
-    #randomizeClothing and Rotate Bones
-    randomize(Hand)
-    
-    resetBones(Hand.rig)
-    rotateBones(Hand.rig)
-    
-    
-    
-    
-    
-    
-    #hide it again
-    #hideHand(HandIndex) ##-----------------------------------------------------------uncomment when run in loop
-
-
-
-#------------------------------------------------------------------------------------------------------------ main loop
-
 #rotates a single bone within the limits of the constaint
 def rotateBone(self):
     self.rotation_mode = 'XYZ'
@@ -223,13 +194,6 @@ def resetBones(Rig):
         except:
             pass
         
-#rotates all bones, renders the image and resets the bones
-def rotateAndRender(counter, Rig):
-    rotateBones(Rig)
-    renderImage(counter)
-    exportLabels(counter, Rig)
-    resetBones(Rig)
-
 #renders image and saves it at the given path at the top of the script
 def renderImage(counter):
     bpy.context.scene.camera = bpy.data.objects["Camera"]
@@ -265,48 +229,66 @@ def exportLabels(counter, Rig):
 
 
 
+#def export():
+    #export labels: right/left, hasWatch(bool), hasRing(bool), hasSleeve(bool)
+    #export labels for each bone(position, rotation??)
+    #render image and save
+    #render tiefenbild?
+    
+def showAll():
+    for x in HandList:
+        x.collection.hide_viewport = False 
+
+def showAllC():
+    for col in bpy.data.collections:
+        col.hide_viewport = False
+
+#the main method
+def loop():
+    index = StartAt
+    #choose one Hand random
+    for index in range(StartAt, NumberOfPoses):
+        
+        temp = len(HandList)
+        HandIndex = randrange(temp)
+        Hand = HandList[HandIndex]
+        #unhide Hand, cloth, ring and watch in render
+        unhideOne(Hand)
+    
+        #randomize ring, watch and clothes
+        randomize(Hand)
+        #rotate the Bones of the rig
+        rotateBones(Hand.rig)
+    
+        renderImage(index)
+        #exportLabels(counter, Hand.rig)
+    
+        #showAll()
+        #showAllC()
+        resetBones(Hand.rig)
+        #hide it again
+        hideHand(Hand) 
+
+
+
+#------------------------------------------------------------------------------------------------------------ main loop
+
+
+
+
 def main():
-    #select ActiveRig
-    ActiveRig = getActiveRig()
     
     #set ARig to active Object to make sure that enabling Pose Mode is possible
     bpy.context.view_layer.objects.active = ActiveRig
     #make sure to be in POSE mode, so the rotation will show in the rendered Image(in Object mode you won't see the difference)
     bpy.ops.object.mode_set(mode='POSE')
     
-    #setting the Path Variables
-    #needs to be global to get the FilePath Varibale from the top. leaving out "global" would create a new local Variable
-    global FilePath
-    FilePath = Directory + '/' + ActiveRig.name
-    global ImageFilePath
-    ImageFilePath = FilePath + '/images' #directory where the images will be saved
-    global LabelsFilePath
-    LabelsFilePath = FilePath + '/labels' #directory where the labelfiles will b saved
-    
-    #creates needed Directorys
-    createDirectory(FilePath)
-    createDirectory(LabelsFilePath)
-    createDirectory(ImageFilePath)
     
     #loop for the wanted number of images
     for i in range(StartAt, StartAt + NumberOfPoses):
         rotateAndRender(i, ActiveRig)
         print(i)
-        
-def hideExcept(Hand):
-    for x in HandList:
-        x.collection.hide_viewport = True
-    try:       
-        Hand.collection.hide_viewport = False
-    except:
-        print('Index out of Bounds')
-        
-def testing():
-    initHands()
-    L = bpy.context.view_layer
-    hideExcept(HandList[1])
-    
-    
+           
 init()
 loop()
 
